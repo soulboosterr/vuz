0c2ab1cea90ad4ff1182c09975edf5455bc73673
aversinit <reprek2@gmail.com>
2023-09-15 10:04:53 +0300

    '

--- conspect lecture/git.py
+++ conspect lecture/git.py
@@ -5,13 +5,16 @@ git status
 
 2
 git commit - m 'commentary'
-ahaha
+
 3
 git reset --hard i
 git revert i
 
 4
-
+git checkout -b name_branch
+git branch -m old new
+git checkout name_branch
+git branch -d name
 
 
 
